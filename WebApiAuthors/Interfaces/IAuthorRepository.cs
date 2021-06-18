3f5812f2e551ae075ff00129508b3bd62b8d03f0
BrVenturaU <brandonventura16@gmail.com>
2021-06-17 20:21:26 -0600

    Agrega servicio de repositorio de autores

---
+++ WebApiAuthors/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAuthors.Entities;
+
+namespace WebApiAuthors.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        Task<List<Author>> GetAllAuthors();
+        Task<Author> GetFirstAuthor();
+        Task<Author> GetAuthorById(int id);
+        Task<Author> GetAuthorByName(string name);
+        Task<List<Book>> GetAuthorBooks(int authorId);
+        Task<string> CreateAuthor(Author author);
+        Task<string> UpdateAuthor(Author author, int id);
+        Task<string> DeleteAuthor(int id);
+    }
+}
---
+++ WebApiAuthors/Services/AuthorRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAuthors.Entities;
+using WebApiAuthors.Interfaces;
+
+namespace WebApiAuthors.Services
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        public Task<string> CreateAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteAuthor(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Author>> GetAllAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Book>> GetAuthorBooks(int authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Author> GetAuthorById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Author> GetAuthorByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Author> GetFirstAuthor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateAuthor(Author author, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
